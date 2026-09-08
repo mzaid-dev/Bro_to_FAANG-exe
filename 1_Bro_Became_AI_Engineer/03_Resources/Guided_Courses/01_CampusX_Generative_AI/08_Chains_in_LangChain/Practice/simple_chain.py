05bbda8726a44dd953c05deaa72b86611416bfba
mzaid-dev <dev.mzaid@gmail.com>
2026-09-08 18:53:38 +0500

    feat

--- 1_Bro_Became_AI_Engineer/03_Resources/Guided_Courses/01_CampusX_Generative_AI/08_Chains_in_LangChain/Practice/simple_chain.py
+++ 1_Bro_Became_AI_Engineer/03_Resources/Guided_Courses/01_CampusX_Generative_AI/08_Chains_in_LangChain/Practice/simple_chain.py
@@ -21,3 +21,29 @@ chain = template | client | parser
 print(chain.invoke({'topic':'LLM evaluation'}))
 
 print(chain.get_graph().draw_ascii)
+
+
+
+from model import client
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+parser = StrOutputParser()
+
+template = PromptTemplate(
+    template="Generate 5 interesting fact about {topic}",
+    input_variables=['topic']
+)
+
+# without chain 
+# prompt = template.invoke({"topic":"cricket"})
+# result = client.invoke(prompt)
+# final_result = parser.invoke(result)
+
+# with chain 
+
+chain = template | client | parser
+
+print(chain.invoke({'topic':'LLM evaluation'}))
+
+print(chain.get_graph().draw_ascii)
